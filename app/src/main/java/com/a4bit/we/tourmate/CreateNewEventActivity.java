2d487b986c873dd7a8894e1baa1abeb1a9aed8cc
mominworkbd <momin.work.bd@gmail.com>
2016-08-22 16:26:15 +0600

    Google map activity added

--- app/src/main/java/com/a4bit/we/tourmate/CreateNewEventActivity.java
+++ app/src/main/java/com/a4bit/we/tourmate/CreateNewEventActivity.java
@@ -1,12 +1,21 @@
 package com.a4bit.we.tourmate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateNewEventActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_event);
+    }
+
+    public void openGoogleMap(View view) {
+
+        Intent intent =new Intent(this, SelectPlaceMapsActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/a4bit/we/tourmate/MainActivity.java
+++ app/src/main/java/com/a4bit/we/tourmate/MainActivity.java
@@ -1,13 +1,26 @@
 package com.a4bit.we.tourmate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button goToCreateNewEventActivityMainActivityBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goToCreateNewEventActivityMainActivityBTN = (Button) findViewById(R.id.goToCreateNewEventActivityViewBTN);
+    }
+
+    public void goToCreateNewEventActivity(View view) {
+
+        Intent intent =new Intent(this, CreateNewEventActivity.class);
+        startActivity(intent);
     }
 }
